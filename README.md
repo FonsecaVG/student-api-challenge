3a55ff76936068c4c57493c30567af051326c11a
VictorFonseca <vfonseca.dev@gmail.com>
2025-10-14 18:50:12 -0300

    feat: complete student API solution and validation logic

---
+++ README.md
@@ -0,0 +1,43 @@
+# Student API Challenge - Flask/Python
+
+## ✨ Endpoints Funcionais
+
+* **POST /students:** Cadastra estudantes com validação obrigatória da nota (entre 0 e 10).
+* **GET /students:** Retorna a lista completa de todos os estudantes.
+* **GET /students/:id:** Retorna os dados de um estudante específico (ou 404 Not Found).
+
+## 🧠 Justificativa das Decisões (Decisions Made)
+
+
+Justificativa decisões:
+
+- **Armazenamento de Dados e Estrutura:**
+- Justificativa: Armazenamento feito por lista Python global para manter o código em único arquivo (app.py).
+
+- **Lógica do Caractere Único (get_unique_char):**
+- Justificativa: Após filtrar o nome para garantir que seja somente string e todas minúsculas (lower case), decidi usar função count do Python dentro do loop para contar e validar o nº de ocorrência da letra.
+
+- **JSON e Validação:**
+- Justificativa: O Flask é usado para retornar o formato JSON e aplicar a validação da nota.
+O uso do jsonify garante que a API siga o padrão RESTful. A validação numérica da nota com (try/except) prova a capacidade de lidar com entradas inválidas do cliente de forma controlada, retornando mensagens de erro claras (400 Bad Request).
+
+- **Verificação de Qualidade**
+- Teste: Por fim, a funcionalidade da API foi validada usando a extensão Thunder Client no VS Code para simular todas as requisições POST e GET, garantindo a precisão da lógica de retorno do caractere único e a correta aplicação dos códigos HTTP (201, 200, 400, 404).
+
+
+## 🚀 Run Locally
+1. Clone o repositório:
+    ```bash
+    git clone [SEU_LINK]
+    cd student-api-challenge
+    ```
+2. Crie o ambiente virtual e instale:
+    ```bash
+    python -m venv venv
+    .\venv\Scripts\activate  # Windows
+    pip install -r requirements.txt
+    ```
+3. Inicie a API:
+    ```bash
+    python app.py
+    ```
--- app.py
+++ app.py
@@ -1,12 +1,113 @@
 from flask import Flask, request, jsonify
 
+# List to simulate the database
+students_list = []
+
+# Variables for sequencial IDs
+next_student_id = 1
+
+
 app = Flask(__name__)
 
 
-@app.route('/students', methods='POST')
+# Logic Function: finds first non-repeating char
+def get_unique_char(name):
+
+    # Converts to lowercase and only alpha chars
+    filtered_name = ''.join(c.lower() for c in name if c.isalpha())
+    if not filtered_name:
+        return '_'
+    
+    for char in filtered_name:
+        if filtered_name.count(char) == 1:
+            return char
+    return "_"
+
+# POST /students: Add a new student
+@app.route('/students', methods=['POST'])
 def register_student():
+    global next_student_id
+
+    data = request.get_json()
+    # JSON data validation
+    if not data:
+        return jsonify({'error': 'Missin JSON data'}), 400
+
+    student_name = data.get('name')
+    student_grade = data.get('grade')
+
+    # Fields validation
+    if not student_name or student_grade is None:
+        return jsonify({'error': 'Missing "name" or "grade"'}), 400
+    
+    # Ensures grade between 0 and 10.
+    try:
+        student_grade = float(student_grade)
+        if not(0<= student_grade <=10):
+            return jsonify({'error': 'Grade must be between 0 and 10'}), 400
+    except (ValueError, TypeError):
+
+        return jsonify({'error': 'Grade must be a valid number'})
+    
+    # Creates a new
+    new_student = {
+        'id': next_student_id,
+        'name': student_name,
+        'grade': student_grade,
+        'unique_char': get_unique_char(student_name)
+    }
 
 
+    # Store new_student in the list.
+    students_list.append(new_student)
+    next_student_id += 1
 
-@app.route('/students', methods='POST')
-def regis():
+    return jsonify(new_student), 201
+
+# GET /student: Returns a list of all students
+@app.route('/students', methods=['GET'])
+def list_students():
+
+    # Prepares de response list and recalculate the 'unique_char'
+    response_list = [
+        {
+        'id': s['id'],
+        'name': s['name'],
+        'note': s['grade'],
+        'unique_char': get_unique_char(s['name'])
+    }
+    for s in students_list ]
+
+    # Returns the updated list as JSON with 200(OK) status 
+    return jsonify(response_list), 200
+
+# GET /students/<int:student_id>: Returns data for a specific student
+@app.route('/students/<int:student_id>', methods=['GET'])
+def get_student(student_id):
+    
+    id_found = None
+    
+    # Search for students ID
+    for s in students_list:
+        if s['id'] == student_id:
+            id_found = s
+            break
+    
+    # Verify if ID is not None.
+    if id_found is None:
+        return jsonify({'error': f'Student with ID {student_id} not found'}), 404
+
+    # Response prep: Ensure the response includes all fields
+    response_data = {
+        'id': id_found['id'],
+        'name': id_found['name'],
+        'grade': id_found['grade'],
+        'unique_char': get_unique_char(id_found['name'])
+    }
+
+    return jsonify(response_data), 200
+
+
+if __name__ == '__main__':
+# Starts the Flask development server
+    app.run(debug=True)
