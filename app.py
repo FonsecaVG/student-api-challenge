3ab7b299f351ad0d9b603b2c43098a9b19cccd98
VictorFonseca <vfonseca.dev@gmail.com>
2025-10-14 15:54:41 -0300

    feat: initial setup for student API challenge

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/students', methods='POST')
+def register_student():
+
+
+
+@app.route('/students', methods='POST')
+def regis():
